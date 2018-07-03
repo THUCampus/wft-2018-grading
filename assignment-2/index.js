f2d14308e9ee6a5e3a5324f3c5c105284d7481a1
Li Zhaoyang <lizy14@yeah.net>
2018-07-03 10:21:29 +0800

    基础实验2：2018年7月2日网络学堂发布版本

---
+++ assignment-2/index.js
@@ -0,0 +1,35 @@
+/*
+
+Please feel free to modify the existing codes.
+
+All functions (or classes) to be be submitted should be in global scope.
+
+If you have any question, feel free to ask in the course's wechat group.
+
+*/
+
+'use strict';
+
+function ip_sort(ip_addresses) {
+	// TODO:
+	return ip_addresses;
+}
+
+class TreeIterator {
+	constructor(root) {
+		// TODO:
+	}
+	has_next() {
+		// TODO:
+		return false;
+	}
+	next_value() {
+		// TODO:
+		return null;
+	}
+}
+
+function equal(o1, o2) {
+	// TODO:
+	return true;
+}
